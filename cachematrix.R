a911f5cc68ba0d7953f76be5076a983746967fbd
Moto Kuroi <motoharu.kuroi@gmail.com>
2015-08-23 17:58:17 +0900

    almost?

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+## List can handle matrix, too.  So, it is basically the same logic as the example.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, 
+       setsolve = setsolve, 
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## As the example showed, the list contains x and solve(x) can answer the question without computing.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+
